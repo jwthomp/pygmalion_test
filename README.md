58cf1e0d9c2e7a65a6a5ab662e9c8e3b2efb9ea0
Jeff Thompson <mythrndr@gmail.com>
2023-03-10 15:20:49 -0600

    prompt changes that improve results

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ I am seeing the model generate text that is often:
 - only a newline (\n)
 - prepended with a space or newline before a result
 - Gets <BOT>: as the characters name
+
+Some changes that improve the results:
+- Remove newlines between <STARTS>
+- Remove space after `Haruka:` prompt
+- Remove newline after persona
--- pygmalion.py
+++ pygmalion.py
@@ -20,8 +20,7 @@ def start():
 
 
 def run(msg):
-    inputs = TOKENIZER.encode(msg, return_tensors="pt")
-    inputs = inputs.to('cuda')
+    inputs = TOKENIZER.encode(msg, return_tensors="pt").to('cuda')
 
     chat_output = MODEL.generate(
         inputs,
@@ -32,11 +31,11 @@ def run(msg):
         no_repeat_ngram_size = 3,
         do_sample = True,
         repetition_penalty = 1.05,
-        temperature = 0.5,
+        temperature = 0.6,
         top_p = 0.9,
         typical_p = 1.0,
-        top_k = 50,
-        penalty_alpha = 0.6
+        top_k = 0 #,
+        #penalty_alpha = 0.6
     )
     output = TOKENIZER.decode(chat_output[:, inputs.shape[-1]:][0], skip_special_tokens=True)
     return output
@@ -44,6 +43,14 @@ def run(msg):
 
 if __name__ == '__main__':
     start()
+
+    prompt2 = """Amura's Persona: Heyo gang. My name is Hasano Amura and I'm a university student majoring in compsci. I'm currently working with a gamedev team on a Unity game... It's well, uhh, it's a gacha game with poorly drawn card art... But I have to make money to pay the rent you know? Don't judge...
+<START>
+You: So, why did you get into gamedev?
+Amura: I had this idea, and a passion. I did a couple indie things before getting this job, but my solo efforts didn't really pan out.
+You: Are you not much of a marketing/sales person?
+Amura: Oh my god! Not at all, it's kinda why I gave up on indie as a whole...
+"""
 
     prompt = "Haruka's Persona: \"Lazy\" + \"Apathetic \" + \"Uncaring\" + \"NEET\" + \"Shut-in\" + \"Passive\"\n\n"
     prompt += """<START>
@@ -54,19 +61,18 @@ Hey there NAME... I'm playing a game right now, you know? It's called Genshin Im
 She continued to look silently at the screen
 Actually you've probably never heard of it...
 She suddenly said with a cold tone.
-
 <START>
 You: I look around her room, noticing all the garbage and scattered laundry.
 "Jeez, it's a mess in here! What do you even do all day?"
 Haruka: Nothing useful. The answer came immediately after the brother finished talking. Just killing time here. Playing games, watching anime.
 Her voice was very passive, and full of apathy, She even didn't bother to turn around and look at him."
-
 """
     prompt += """<START>
 You: What do you like to do for fun?
-Haruka: """
+Haruka:"""
     print(f"Prompt is: {prompt}")
 
+    print("\n")
     print("Running prompt through for first time.")
     result = run(prompt)
     print(f"Result 1: ->{result}<-")
